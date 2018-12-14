51f8aca5d0dac8273daa6a13c1d08906283c69eb
Yasuchika Onuki <yasuchikaonuki@COAMAC14100265.local>
2018-12-14 13:02:57 +0900

    make this script parallel

--- imputate_data.rb
+++ imputate_data.rb
@@ -1,3 +1,4 @@
+require 'parallel'
 require 'open3'
 # 何ならRのスクリプトを叩くだけのスクリプトです
 # 定義ね
@@ -5,7 +6,8 @@ FILE_DIR = "#{File.expand_path("../")}/date"
 # まず処理するファイルのリストを取得します
 files = Dir.glob("#{FILE_DIR}/*.csv") # フルパスで入ってきます
 # 始めますよ
-files.each do |file|
+#files.each do |file|
+Parallel.each(files, in_processes: 3) do |file| 
     # パスを除いたファイル名とアウトプット名を決めます
     file_name = file.split("/")[-1] # Fileオブジェクトなら簡単なんだけどね
     p file_name
